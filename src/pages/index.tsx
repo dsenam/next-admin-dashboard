9ef62d91586e9f548c5f69088cf959e335013a76
Douglas Sena <dsena.m@hotmail.com>
2022-03-22 11:16:46 -0300

    visual studio code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,48 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Flex, Input, Button, Stack } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <h1>Dash</h1>
-  )
+    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <Input
+            name="email"
+            type="email"
+            focusBorderColor="pink.500"
+            bgColor="gray.900"
+            variant="filled"
+            _hover={{
+              bgColor: "gray.900",
+            }}
+            size="lg"
+            placeholder="E-mail"
+          />
+          <Input
+            name="password"
+            type="password"
+            focusBorderColor="pink.500"
+            bgColor="gray.900"
+            variant="filled"
+            _hover={{
+              bgColor: "gray.900",
+            }}
+            size="lg"
+            placeholder="Password"
+          />
+        </Stack>
+
+        <Button type="submit" marginTop="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
