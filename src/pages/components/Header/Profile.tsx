1c347a7613a9da37ba315d5aad61991df717ab96
Douglas Sena <dsena.m@hotmail.com>
2022-03-23 12:32:31 -0300

    + responsive drawer

--- src/pages/components/Header/Profile.tsx
+++ src/pages/components/Header/Profile.tsx
@@ -1,20 +1,28 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
-    return (
-        <Flex align="center">
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
+  return (
+    <Flex align="center">
+      {showProfileData && (
+        <>
           <Box mr="4" textAlign="right">
             <Text>Douglas Sena</Text>
             <Text color="gray.300" fontSize="small">
               dsena@rocketseat
             </Text>
           </Box>
+        </>
+      )}
 
-          <Avatar
-            size="md"
-            name="Douglas Sena"
-            src="https://github.com/dsenam.png"
-          />
-        </Flex>
-    )
+      <Avatar
+        size="md"
+        name="Douglas Sena"
+        src="https://github.com/dsenam.png"
+      />
+    </Flex>
+  );
 }
